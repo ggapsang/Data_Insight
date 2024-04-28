327a9e74e5962b7ea451f09f2a81b103ca02037d
sanghyun lee <58983068+ggapsang@users.noreply.github.com>
2024-04-28 22:28:53 +0900

    Update process_pdf_files.py

--- pyForData/process_pdf_files.py
+++ pyForData/process_pdf_files.py
@@ -3,7 +3,28 @@ import re
 import os
 import pandas as pd
 import ezdxf
+import pdfplumber
 
+def extract_coor(pdf_path) :
+    """pdf 안의 텍스트들의 단어들의 좌표 추출"""
+    with pdfplumber.open(pdf_path) as pdf :
+        first_page = pdf.pages[0]
+        text_data = first_page.extract_words()
+
+    for word in text_data:
+        print(f"Text: {word['text']}, Coordinates: ({word['x0']}, {word['top']}, {word['x1']}, {word['bottom']})")
+        # x0: 텍스트 시작 위치의 x, top : 텍스트 시작 위치의 y, x1 텍스트 끝 위치의 x, bottom 텍스트 끝 위치의 y
+
+def extract_text_by_coor(pdf_path, coor : tuple) :
+    "pdf 안의 특정 좌표를 기준으로 사각형 영역 내의 텍스트를 추출함"
+    with pdflpmber.open(pdf_path) as pdf :
+        first_page = pdf.pages[0]
+        cropping_area = coor
+        cropped_page= first_page.crop(bouding_box=cropping_area)
+        extract_text = cropped_page.extarct_text()
+
+    return extract_text
+        
 def extract_annotations_from_pdf(pdf_path) :
     """pdf 파일에서 주석 추출"""
     doc = fitz.open(pdf_path)
